5659dc3894e17a22b2fad553b3ace6110e95c3ec
João Robalo <joao27of@gmail.com>
2022-11-24 22:56:07 -0300

    Adicionando novos eventos de clique nos botões de conclusão ou remoção de tarefa

--- js/script.js
+++ js/script.js
@@ -49,3 +49,16 @@ toDoForm.addEventListener("Submit", (e) => {
     saveToDo(inputValue);
   }
 });
+
+document.addEventListener("click", (e) => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("div");
+
+  if (targetEl.classList.contains("finish-to-do")) {
+    parentEl.classList.toggle("done");
+  }
+
+  if (targetEl.classList.contains("remove-to-do")) {
+    parentEl.remove();
+  }
+});
